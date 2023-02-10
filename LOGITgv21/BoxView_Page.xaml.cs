7a8c1f2c1ceb933c17b55e428fe29142adcbf1a0
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2023-02-10 10:35:15 +0200

    BoxView_Page

---
+++ LOGITgv21/BoxView_Page.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LOGITgv21
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BoxView_Page : ContentPage
+    {
+        public BoxView_Page()
+        {
+            BoxView box = new BoxView
+            {
+                Color = Color.FromRgb(0, 0, 0),
+                CornerRadius = 15,
+                WidthRequest = 100,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.Center
+
+            };
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = { box }
+            };
+            Content= st;
+        }
+    }
+}
